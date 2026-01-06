2da8941d282f27bab85914b07f63a5a29cbcb101
Ambrose Blay <ab2838@cornell.edu>
2026-01-06 12:31:57 -0500

    implement get_diff

--- src/app.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-# Execute the 'echo' command and capture its output
-result = subprocess.run(["git", "diff"], capture_output=True, text=True)
-print(f"Output: {result.stdout}")
-print(f"Error: {result.stderr}")
